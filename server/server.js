8dae28b09992b68e822043d6834ed78c89188e0a
pabloc33 <pablo.contino33@gmail.com>
2021-07-29 19:50:58 -0300

    RestServer - usuario

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require("./config/config");
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 // sin instalar body-parser, podemos usar las siguientes línea
@@ -8,35 +9,21 @@ app.use(express.urlencoded({ extended: false }));
 // parse application/json
 app.use(express.json());
 
-app.get("/usuario", function (req, res) {
-  res.json("get Usuario");
-});
+app.use(require("./routes/usuario"));
 
-app.post("/usuario", function (req, res) {
-  let body = req.body;
-
-  if (body.nombre === undefined) {
-    res.status(400).json({
-      ok: false,
-      message: "El nombre es necesario",
-    });
-  } else {
-    res.json({
-      persona: body,
-    });
-  }
-});
-
-app.put("/usuario/:id", function (req, res) {
-  let id = req.params.id;
-  res.json({
-    id,
+mongoose
+  .connect(process.env.URLDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("Base de datos ONLINE");
+  })
+  .catch((err) => {
+    console.error(err);
   });
-});
-
-app.delete("/usuario", function (req, res) {
-  res.json("delete Usuario");
-});
 
 app.listen(process.env.PORT, () => {
   console.log("Escuchando puerto: ", process.env.PORT);
